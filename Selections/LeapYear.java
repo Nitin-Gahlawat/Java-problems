40a9cb02a96c621c147f0ad3be24e5080130d424
Nitin-Gahlawat <gahlawatnitin03@gmail.com>
2023-08-24 20:34:44 +0530

    WAP to Determine weather the year is a leap year or not.

---
+++ Selections/LeapYear.java
@@ -0,0 +1,13 @@
+//Question
+    //WAP to Determine weather the year is a leap year or not.
+//Answer
+import javax.swing.JOptionPane;
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the year to cheack"));
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            JOptionPane.showMessageDialog(null, "The Year " + year + " is a leap year");
+        } else
+            JOptionPane.showMessageDialog(null, "The Year " + year + " is a not leap year");
+    }
+}
